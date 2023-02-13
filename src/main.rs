460da5fc76d240e11537a7a4db47304389d9fc6d
Cristyan Valera <cristyan.valera.81@gmail.com>
2023-02-12 22:02:12 -0400

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
-use std::{io, process, error::Error};
+use std::{error::Error, process, ffi::OsString, env};
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let file_path = get_first_arg()?;
+    let mut rdr = csv::Reader::from_path(file_path)?;
+
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+
+    Ok(())
+}
+
+fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("Expected 1 argument, but got none.")),
+        Some(file_path) => Ok(file_path),
+    }
+}
 
 fn main() {
     if let Err(err) = run() {
         eprintln!("{}", err);
         process::exit(1);
     }
-}
-
-fn run() -> Result<(), Box<dyn Error>> {
-    let mut rdr = csv::Reader::from_reader(io::stdin());
-
-    for result in rdr.records() {
-       let record = result?;
-       println!("{:?}", record);
-    }
-
-    Ok(())
 }
