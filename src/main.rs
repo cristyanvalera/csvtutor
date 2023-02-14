375b35c1054422857ebe020b65f58ba91e89b457
Cristyan Valera <cristyan.valera.81@gmail.com>
2023-02-13 21:11:17 -0400

    add ReaderBuilder set of choices for parse a cvs file

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,38 @@
-use std::{error::Error, process, ffi::OsString, env};
+use std::{error::Error, process, io};
 
 fn run() -> Result<(), Box<dyn Error>> {
-    let file_path = get_first_arg()?;
-    let mut rdr = csv::Reader::from_path(file_path)?;
+    // let file_path = get_first_arg()?;
+    // let mut rdr = csv::Reader::from_path(file_path)?;
+
+    // for result in rdr.records() {
+    //     let record = result?;
+    //     println!("{:?}", record);
+    // }
+
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .delimiter(b';')
+        .double_quote(false)
+        .escape(Some(b'\\'))
+        .flexible(true)
+        .comment(Some(b'#'))
+        .from_reader(io::stdin());
 
     for result in rdr.records() {
         let record = result?;
+
         println!("{:?}", record);
     }
 
     Ok(())
 }
 
-fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
-    match env::args_os().nth(1) {
-        None => Err(From::from("Expected 1 argument, but got none.")),
-        Some(file_path) => Ok(file_path),
-    }
-}
+// fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
+//     match env::args_os().nth(1) {
+//         None => Err(From::from("Expected 1 argument, but got none.")),
+//         Some(file_path) => Ok(file_path),
+//     }
+// }
 
 fn main() {
     if let Err(err) = run() {
