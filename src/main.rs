c8a63d9d7fa42df4df4b9b8a8220b33e69e5152b
Cristyan Valera <cristyan.valera.81@gmail.com>
2023-02-13 21:14:33 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
 use std::{error::Error, process, io};
 
 fn run() -> Result<(), Box<dyn Error>> {
-    // let file_path = get_first_arg()?;
-    // let mut rdr = csv::Reader::from_path(file_path)?;
-
-    // for result in rdr.records() {
-    //     let record = result?;
-    //     println!("{:?}", record);
-    // }
-
     let mut rdr = csv::ReaderBuilder::new()
         .has_headers(false)
         .delimiter(b';')
@@ -26,13 +18,6 @@ fn run() -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
-
-// fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
-//     match env::args_os().nth(1) {
-//         None => Err(From::from("Expected 1 argument, but got none.")),
-//         Some(file_path) => Ok(file_path),
-//     }
-// }
 
 fn main() {
     if let Err(err) = run() {
