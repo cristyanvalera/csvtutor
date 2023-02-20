1789277607351f1588c14fb021910999f86947eb
Cristyan Valera <cristyan.valera.81@gmail.com>
2023-02-20 12:25:01 -0400

    read with serde

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,14 @@
 use std::{error::Error, process, io};
 
-// type Record = (String, String, Option<u64>, f64, f64);
+type Record = (String, String, Option<u64>, f64, f64);
 
 fn run() -> Result<(), Box<dyn Error>> {
     let mut rdr = csv::Reader::from_reader(io::stdin());
-    for result in rdr.records() {
-        let record = result?;
-        
-        let city = &record[0];
-        let state = &record[1];
-        let pop: Option<u64> = record[2].parse().ok();
-        let latitude: f64 = record[3].parse()?;
-        let longitude: f64 = record[4].parse()?;
 
-        println!(
-            "city: {:?}, state: {:?}, \
-            pop: {:?}, latitude: {:?}, longitude: {:?}",
-            city, state, pop, latitude, longitude);
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        
+        println!("{:?}", record);
     }
 
     Ok(())
